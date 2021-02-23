5740d7f27ba7e4364ef8b2de79f7d89dd9a70e2f
Lynee Lawson <lyneelawson@gmail.com>
2021-02-23 16:09:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(now_board)
+  puts " #{now_board[0]} | #{now_board[1]} | #{now_board[2]}"
+end
+
+display_board([3,5,6])
